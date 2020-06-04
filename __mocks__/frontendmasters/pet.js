74ff7b0a02d70eba46324327f8ee8099aee481fd
Artiom <sinigur2006@gmail.com>
2020-06-04 11:30:07 -0400

    Create a mock for component SearchParams

---
+++ __mocks__/frontendmasters/pet.js
@@ -0,0 +1,39 @@
+// If a put a file in here called lodash.js, that file will be imported anywhere I have import _ from "lodash"
+
+import { readFileSync } from "fs";
+import path from "path";
+import { act } from "@testing-library/react";
+
+const breeds = [
+  { name: "Bichon Frise" },
+  { name: "Bolognese" },
+  { name: "Bolonka" },
+  { name: "Coton de Tulear" },
+  { name: "Havanese" },
+  { name: "Lowchen" },
+  { name: "Maltese" },
+];
+
+const doggos = JSON.parse(
+  readFileSync(path.join(__dirname, "/res.json")).toString()
+);
+
+export const ANIMALS = ["dog", "cat", "bird"];
+// here underscore "_" means that is only for testing
+export const _breeds = breeds;
+export const _dogs = doggos.animals;
+
+const mock = {
+  breeds: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback({ breeds })),
+    };
+  }),
+  animals: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback(doggos)),
+    };
+  }),
+};
+
+export default mock;
